36664c4164a97116a2bfe4e4845c09cbe878904a
Volker Hänsel <volker@haensel-online.de>
2023-10-12 21:13:36 +0200

    empty config

---
+++ IPS-Zeitschaltuhr/helper/TSW_Config.php
@@ -0,0 +1,88 @@
+<?php
+
+
+declare(strict_types=1);
+
+trait TSW_Config
+{
+    /**
+     * Reloads the configuration form.
+     *
+     * @return void
+     */
+    public function ReloadConfig(): void
+    {
+        $this->ReloadForm();
+    }
+
+    /**
+     * Expands or collapses the expansion panels.
+     *
+     * @param bool $State
+     * false =  collapse,
+     * true =   expand
+     *
+     * @return void
+     */
+    public function ExpandExpansionPanels(bool $State): void
+    {
+        for ($i = 1; $i <= 8; $i++) {
+            $this->UpdateFormField('Panel' . $i, 'expanded', $State);
+        }
+    }
+
+    /**
+     * Modifies a configuration button.
+     *
+     * @param string $Field
+     * @param string $Caption
+     * @param int $ObjectID
+     * @return void
+     */
+    public function ModifyButton(string $Field, string $Caption, int $ObjectID): void
+    {
+        $state = false;
+        if ($ObjectID > 1 && @IPS_ObjectExists($ObjectID)) { //0 = main category, 1 = none
+            $state = true;
+        }
+        $this->UpdateFormField($Field, 'caption', $Caption);
+        $this->UpdateFormField($Field, 'visible', $state);
+        $this->UpdateFormField($Field, 'objectID', $ObjectID);
+    }
+
+    /**
+     * Modifies a trigger list configuration button
+     *
+     * @param string $Field
+     * @param string $Condition
+     * @return void
+     */
+    public function ModifyTriggerListButton(string $Field, string $Condition): void
+    {
+        $id = 0;
+        $state = false;
+        //Get variable id
+        $primaryCondition = json_decode($Condition, true);
+        if (array_key_exists(0, $primaryCondition)) {
+            if (array_key_exists(0, $primaryCondition[0]['rules']['variable'])) {
+                $id = $primaryCondition[0]['rules']['variable'][0]['variableID'];
+                if ($id > 1 && @IPS_ObjectExists($id)) { //0 = main category, 1 = none
+                    $state = true;
+                }
+            }
+        }
+        $this->UpdateFormField($Field, 'caption', 'ID ' . $id . ' Bearbeiten');
+        $this->UpdateFormField($Field, 'visible', $state);
+        $this->UpdateFormField($Field, 'objectID', $id);
+    }
+
+    /**
+     * Gets the configuration form.
+     *
+     * @return false|string
+     * @throws Exception
+     */
+    public function GetConfigurationForm()
+    {
+	}
+}
